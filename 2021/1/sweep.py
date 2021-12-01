4336d99e70aa73ab6baf6c584da8009cbbc52aff
Stefan K <sdwarfs@worldtalk.de>
2021-12-01 19:13:02 +0100

    split task 1/2 of day 1 into two files. recreated code for task 1.

--- 2021/1/sweep.py
+++ 2021/1/sweep.py
@@ -1,11 +1,6 @@
 content = [int(x) for x in open("input.txt")]
 
 count = 0
-for i in range(1,len(content)-2):
-    val1 = val2 = 0
-    for j in range(0,3):
-        val2 += content[i+j]
-        val1 += content[i+j-1]
-    print(val1, " ", val2)
-    if val1 < val2: count += 1
+for i in range(1,len(content)):
+    if content[i-1] < content[i]: count += 1
 print(count)
