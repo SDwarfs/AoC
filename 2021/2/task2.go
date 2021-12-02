84bb99928d547a8d05d10e8d775a05383e088f83
Stefan K <sdwarfs@worldtalk.de>
2021-12-02 11:20:03 +0100

    Go solution for day 2.

---
+++ 2021/2/task2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+    s "strings"
+    "strconv"
+)
+
+
+func getFileAsList(fn string) []string {
+    data, err := ioutil.ReadFile(fn)
+    if err != nil {
+        fmt.Println("Error opening file")
+        os.Exit(1)
+    }
+    str := s.Trim(s.Replace(string(data), "\r", "", -1), "\n")
+    list := s.Split(str, "\n")
+    return list
+}
+
+
+func Atoi(str string) int {
+    val, err := strconv.Atoi(str)
+    if err != nil {
+        fmt.Println("Error converting string to number: ", str)
+        os.Exit(2)
+    }
+    return val
+}
+
+
+func main() {
+    list := getFileAsList("input.txt")
+    depth := 0
+    hpos := 0
+    aim := 0
+    for _, line := range list {
+        arr := s.Split(line, " ")
+        cmd := arr[0]
+        valstr := arr[1]
+        val := Atoi(valstr)
+        if (cmd == "forward") {
+            hpos += val
+            depth += val * aim
+        }
+        if (cmd == "up")      { aim -= val }
+        if (cmd == "down")    { aim += val }
+    }
+    fmt.Println("Depth:", depth, ", H-Pos: ", hpos, ", Product: ", depth*hpos)
+}
