6acb35f35c84c69d92bdb476411d16204432c83a
Stefan K <sdwarfs@worldtalk.de>
2021-12-09 12:30:14 +0100

    Added comments and renamed variables for day 9 to make it nice and readable.

--- 2021/9/task1.py
+++ 2021/9/task1.py
@@ -1,16 +1,33 @@
+# read all input lines into lines[] and strip off trailing whitespaces
 lines = [x.rstrip() for x in open("input.txt")]
+
+# for each line, thats not empty (i.e. last line of file)
+# create a list containing all character converted to int values
+# and append it to map[]
 map = []
 for line in lines:
     if line != "": map.append([int(x) for x in line])
+
+# estimate size of map: H = height / number of lines, W = width of the lines
 H = len(map)
 W = len(map[0])
-count = 0
+
+# total_risk_level initialized to 0
+total_risk_level = 0
+
+# go through all lines first (=faster because of CPU cache hits/misses)
 for y in range(H):
+    # go thourh all x positions in the line
     for x in range(W):
-        cur = map[y][x]
-        if x > 0   and map[y][x-1]<=cur: continue
-        if x < W-1 and map[y][x+1]<=cur: continue
-        if y > 0   and map[y-1][x]<=cur: continue
-        if y < H-1 and map[y+1][x]<=cur: continue
-        count += 1 + int(cur)
-print(count)
+        # estimate current positions value
+        val = map[y][x]
+        # check if any horizontal/vertical neighbor exists that has
+        # a lower value, if so: skip the rest of the x-loop code
+        if x > 0   and map[y][x-1] <= val: continue
+        if x < W-1 and map[y][x+1] <= val: continue
+        if y > 0   and map[y-1][x] <= val: continue
+        if y < H-1 and map[y+1][x] <= val: continue
+        # since there are no such neighbors, we found a local minimum
+        # and can add the risk level (1 + val) to the sum.
+        total_risk_level += 1 + val
+print(total_risk_level)
--- 2021/9/task2.py
+++ 2021/9/task2.py
@@ -1,29 +1,55 @@
 import numpy as np
 
+# read all input lines into lines[] and strip off trailing whitespaces
 lines = [x.rstrip() for x in open("input.txt")]
+# for each line, thats not empty (i.e. last line of file)
+# create a list containing all character converted to int values
+# and append it to map[]
 map = []
 for line in lines:
     if line != "": map.append([int(x) for x in line])
+# estimate size of map: H = height / number of lines, W = width of the lines
 H = len(map)
 W = len(map[0])
 
+# create a "done" map of the same size of type boolean, intialized with False
+# to mark any map positions that have already been processed.
 done = np.full((H,W), False, dtype=bool)
 
+# recursive flood fill algorithm, which takes a starting point (x,y)
+# and fills any not yet "done" marked positions without crossing 9 values
+# on the map. It return the size of the filled area.
 def floodfill(x,y):
-    if map[y][x] == 9 or done[y][x] == True: return 0
+    # if we are at a "9" on the map or have processed
+    # this position already, abort and return a size of 0
+    if map[y][x] == 9 or done[y][x]: return 0
+    # mark this position as done
+    # and start with a size 1 (this position)
     done[y][x] = True
     size = 1
+    # recursively go left/right/up/down, and if not out of bounds,
+    # add the returned size to the filled area size to be returned.
     if x > 0:   size += floodfill(x-1, y  )
     if y > 0:   size += floodfill(x,   y-1)
     if x < W-1: size += floodfill(x+1, y  )
     if y < H-1: size += floodfill(x,   y+1)
     return size
 
+# initialize list to store the sizes
 sizes = []
+
+# go through all map positions (lines first, better caching)
 for y in range(H):
     for x in range(W):
-        if map[y][x] < 9 and not done[y][x]:
+        # check if map position is not "9" or already done
+        # else we end up with lots of 0 sizes in the array
+        if map[y][x] == 9 or done[y][x]:
+            # we found an unprocessed position that is not "9"
+            # and hence part of a basin. Floodfill it and add
+            # the returned size to the list.
             sizes.append(floodfill(x,y))
 
+# sort estimated basin sizes in reversed order (largest values first)
 sizes.sort(reverse=True)
+# output product of the first 3 values of the list
 print(np.prod(sizes[0:3]))
