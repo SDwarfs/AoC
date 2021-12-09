84226c3398243ddd5a43154446276aacdb3fcf9c
Stefan K <sdwarfs@worldtalk.de>
2021-12-09 11:55:15 +0100

    Optimized day 9, task #1

--- 2021/9/task1.py
+++ 2021/9/task1.py
@@ -1,37 +1,16 @@
-import numpy as np
-
 lines = [x.rstrip() for x in open("input.txt")]
-#print(lines)
-H = len(lines)
-W = len(lines[0])
 map = []
-for y in range(H):
-    if len(lines[y])==0:
-        H = y
-        break
-    line = []
-    for x in range(W):
-        line.append(int(lines[y][x]))
-    map.append(line)
-W = len(map[0])
+for line in lines:
+    if line != "": map.append([int(x) for x in line])
 H = len(map)
-print(W, H)
+W = len(map[0])
 count = 0
 for y in range(H):
     for x in range(W):
-        ok = True
         cur = map[y][x]
-        #print(x,y,cur, end="|")
-        if x > 0 and map[y][x-1]<=cur:
-            ok = False
-        if x < W-1 and map[y][x+1]<=cur:
-            ok = False
-        if y > 0 and map[y-1][x]<=cur:
-            ok = False
-        if y < H-1 and map[y+1][x]<=cur:
-            ok = False
-        if ok:
-            count += 1 + int(cur)
-            print(x,y,"->", cur)
-print("END")
+        if x > 0   and map[y][x-1]<=cur: continue
+        if x < W-1 and map[y][x+1]<=cur: continue
+        if y > 0   and map[y-1][x]<=cur: continue
+        if y < H-1 and map[y+1][x]<=cur: continue
+        count += 1 + int(cur)
 print(count)
