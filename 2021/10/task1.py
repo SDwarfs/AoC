d83a860adcf96c4bd4c1886eabc060fa4ca21c05
Stefan K <sdwarfs@worldtalk.de>
2021-12-10 07:37:23 +0100

    Reworked code for day 10.

--- 2021/10/task1.py
+++ 2021/10/task1.py
@@ -1,29 +1,32 @@
-import numpy as np
-
+# read lines from file and strip whitespaces at the end
 lines = [x.rstrip() for x in open("input.txt")]
 
-points = 0
-for line in lines:
+# matching closing parenthesis
+matching = { '(': ')', '[': ']', '{': '}',  '<': '>'   }
+# points for synthax errors with these expected closing parenthesis
+points   = { ')': 3,   ']': 57,  '}': 1197, '>': 25137 }
+
+
+def lineScore(line):
+    # reset the list of expected closing parenthesis
     open = []
     for c in line:
-        if c == '[':
-            open.append("]")
-        elif c == '(':
-            open.append(")")
-        elif c =='{':
-            open.append("}")
-        elif c == '<':
-            open.append(">")
-        elif c == ']' or c == ")" or c == "}" or c == '>':
-            e = open[len(open)-1]
-            if c != e:
-                print("corrupted", line, e, "exprected", c, "found")
-                if c == ')': points += 3
-                if c == ']': points += 57
-                if c == '}': points += 1197
-                if c == '>': points += 25137
-                break
+        if c in matching:
+            # if its one of the opening parenthesis
+            # add the matching closing one to the list
+            open.append(matching[c])
+        else:
+            # if not it's a closing one, and needs to match the last
+            # opened one... which needs to be closed first
+            expected = open[-1]
+            # if not expected, return score for this faulty character
+            if c != expected: return points[c]
+            # else: remove correctly closed parenthesis and go on
             open = open[:-1]
-    if len(open) > 0:
-        print("incomplete")
-print(points)
+    # no error: 0 score
+    return 0
+
+# calculate the sum of line scores for all lines
+# and print it
+score = sum([lineScore(line) for line in lines])
+print(score)
--- 2021/10/task2.py
+++ 2021/10/task2.py
@@ -1,39 +1,41 @@
-import numpy as np
+import statistics
 
+# read lines from file and strip whitespaces at the end
 lines = [x.rstrip() for x in open("input.txt")]
 
-linescores = []
-for line in lines:
+# matching closing parenthesis
+matching = { '(': ')', '[': ']', '{': '}',  '<': '>'   }
+# points for synthax errors with these -missing- closing parenthesis
+points   = { ')': 1, ']': 2, '}': 3, '>': 4 }
+
+
+def lineScore(line):
+    # reset the list of expected closing parenthesis
     open = []
-    corrupted = False
     for c in line:
-        if c == '[':
-            open.append("]")
-        elif c == '(':
-            open.append(")")
-        elif c =='{':
-            open.append("}")
-        elif c == '<':
-            open.append(">")
-        elif c == ']' or c == ")" or c == "}" or c == '>':
-            e = open[len(open)-1]
-            if c != e:
-                # print("corrupted", line, e, "exprected", c, "found")
-                corrupted = True
-                break
+        if c in matching:
+            # if its one of the opening parenthesis
+            # add the matching closing one to the list
+            open.append(matching[c])
+        else:
+            # if not it's a closing one, and needs to match the last
+            # opened one... which needs to be closed first
+            expected = open[-1]
+            # if not expected, return 0 score (ignore corrupted lines)
+            if c != expected: return 0
+            # else: remove correctly closed parenthesis and go on
             open = open[:-1]
-    if len(open) > 0 and not corrupted:
-        N = len(open)
-        linescore = 0
-        for x in range(N):
-            c = open[N-x-1]
-            linescore *= 5
-            if c == ')': linescore += 1
-            if c == ']': linescore += 2
-            if c == '}': linescore += 3
-            if c == '>': linescore += 4
-        linescores.append(linescore)
-        print("incomplete", line, open, linescore)
-print(linescores)
-linescores.sort()
-print(linescores[len(linescores)//2])
+    # calculate line score
+    score = 0
+    # go backwards throuth list (last opened to be closed first)
+    for c in reversed(open):
+        score *= 5
+        score += points[c]
+    return score
+
+# get list of all scores
+scores = [lineScore(line) for line in lines]
+# remove 0 scores (no error)
+scores = [x for x in scores if x > 0]
+# output median...
+print(statistics.median(scores))
